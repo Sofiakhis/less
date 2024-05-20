e788c0fb7d87787a74e2d8c3047dacd4de751c12
Sofiakhis <sofiakhismatova@mail.ru>
2024-05-20 16:44:40 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,13 @@
+amount_of_work_completed = (12)
+number_of_hours_spent = (1.5)
+course_name = ('Python')
+time_per_task = (number_of_hours_spent / amount_of_work_completed)
+print(course_name,'всего задач:',amount_of_work_completed, 'затраченно часов:', number_of_hours_spent, 'среднее время выполнения:', time_per_task)
+
+
+
+
+
+
+
+
